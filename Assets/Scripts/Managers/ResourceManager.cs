33bf56badcff04ada9e0bb6dfdce986b812320f6
JOB0811 <34418169+JOB0811@users.noreply.github.com>
2024-08-30 19:09:06 +0900

    Addressable Asset 비동기 로딩 구현, 이후 부터 모든 리소스 관리는 이런식으로 짤수 있도록 해보자, >> 씬전환 로딩창

--- Assets/Scripts/Managers/ResourceManager.cs
+++ Assets/Scripts/Managers/ResourceManager.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using System;
+using Object = UnityEngine.Object;
+using Unity.VisualScripting;
 
 public class ResourceManager : MonoBehaviour
 {
     public static ResourceManager Instance;
+
+    private Dictionary<string, Object> _resources = new Dictionary<string, Object>();
 
     private void Awake()
     {
@@ -17,5 +24,79 @@ public class ResourceManager : MonoBehaviour
             Destroy(this);
         }
         DontDestroyOnLoad(this);
+    }
+    public T Load<T>(string key) where T : Object
+    {
+        if (_resources.TryGetValue(key, out Object resource))
+        {
+            return resource as T;
+        }
+        return null;
+
+    }
+
+    public GameObject Instantiate(string key, Transform parent = null, bool pooling = false)
+    {
+        GameObject prefab = Load<GameObject>($"{key}"); 
+        if(prefab == null)
+        {
+            Debug.Log($"{key} is not found");
+            return null;
+        }
+
+        GameObject poolingObject = Object.Instantiate(prefab, parent);
+        poolingObject.name = prefab.name;
+        return poolingObject;
+    }
+
+    public void Destroy(GameObject gameObject)
+    {
+        if(gameObject == null)
+        {
+            return;
+        }
+        Object.Destroy(gameObject);
+
+    }
+    // 비동기로 리소스를 읽어 오는 함수입니다. 만약 이미 딕셔너리에 리소스가존대한다면 콜백함수만을 호출하고
+    // 그렇지 않다면 Addressables.LoadAssetAsync<T>를 통해 리소스를 읽어옵니다.
+    // 여기서 x는 
+    public void LoadAsync<T>(string key, Action<T> callback = null) where T: Object
+    {
+        if(_resources.TryGetValue(key, out Object resource))  // 딕셔너리에 Key가 존재한다면  resourece를 Object형식으로 반환.
+        {
+            callback?.Invoke(resource as T); // 콜백함수에 리소스를 T타입으로 변환하여 전달
+            return;
+        }
+
+        Addressables.LoadAssetAsync<T>(key).Completed += (x) =>
+        {
+            _resources.Add(key, x.Result);
+            callback?.Invoke(x.Result);
+        };
+        // completed는 비동기 작업이 완료되었을 때 호출되는 이벤트입니다. 
+        // 콜백함수라고 봐도 무방합니다.
+        // x가 ㅇ어떻게 resoure가 되는거야?
+        
+    }
+
+    public void LoadAllAsync<T>(string label, Action<string, int, int > callback) where T : Object
+    {
+        var operationHandle = Addressables.LoadResourceLocationsAsync(label, typeof(T));
+        operationHandle.Completed += (operation) =>
+        {
+            int loadCount = 0;
+            int totalCount = operation.Result.Count;
+
+            foreach (var result in operation.Result)
+            {
+                LoadAsync<T>(result.PrimaryKey, (obj) =>
+                {
+                    loadCount++;
+                    callback?.Invoke(result.PrimaryKey, loadCount, totalCount);
+                });
+                
+            }
+        };
     }
 }
--- Assets/Scripts/Scenes/GameScene.cs
+++ Assets/Scripts/Scenes/GameScene.cs
@@ -5,43 +5,64 @@ using UnityEngine;
 
 public class GameScene : MonoBehaviour
 {
-    public GameObject duckPrefab;
-    public GameObject mushroomPrefab;
-    public GameObject wolfPrefab;
-    public GameObject pigPrefab;
 
-    public GameObject joystricPrefab;
-
-    public GameObject Player;
-    private GameObject player;
+    GameObject duck;
+    GameObject mushroom;
+    GameObject wolf;
+    GameObject pig;
     GameObject joystick;
 
-    private void Awake()
-    {
-        player = Instantiate(Player);      
-    }
+    GameObject player;
+
+
+    /*  private void Awake()
+      {
+          // 리소스 매니저를 통해 비동기로 플레이어 프리팹을 읽어옵니다.
+          // 여기서 람다식의 인자는 리소스 매니저에서 읽어온 리소스를 의미합니다.
+          ResourceManager.Instance.LoadAsync<GameObject>("Player.prefab",
+              (x) =>
+              {
+                  Player = x;
+                  player = Instantiate(Player);
+
+                  player.AddComponent<PlayerController>();
+
+                  Camera.main.GetComponent<CameraController>().target = player.transform;
+              });
+
+
+      }*/
 
     private void Start()
     {
-        GameObject monster = new GameObject("Monster");
-        GameObject duck = Instantiate(duckPrefab);
-        GameObject mushroom = Instantiate(mushroomPrefab);
-        GameObject wolf = Instantiate(wolfPrefab);
-        GameObject pig = Instantiate(pigPrefab);
+        ResourceManager.Instance.LoadAllAsync<GameObject>("Prefabs", (key, index, totalCount) =>
+        {
+            Debug.Log($"Loading {key} {index}/{totalCount}");
 
-        joystick = Instantiate(joystricPrefab);       
+            if (index == totalCount)
+            {
+                Init();
 
+            }
+        });
 
-        
-        duck.transform.SetParent(monster.transform);  
-        mushroom.transform.SetParent(monster.transform);
-        wolf.transform.SetParent(monster.transform);
-        pig.transform.SetParent(monster.transform);
+    }
 
+    void Init()
+    {
 
+        player = ResourceManager.Instance.Instantiate("Player.prefab");
         player.AddComponent<PlayerController>();
-
         Camera.main.GetComponent<CameraController>().target = player.transform;
+
+       joystick = ResourceManager.Instance.Instantiate("Joystick.prefab");
+
+       GameObject monster = new GameObject("Monsters");
+       duck = ResourceManager.Instance.Instantiate("Duck.prefab", monster.transform);
+       mushroom = ResourceManager.Instance.Instantiate("Mushroom.prefab", monster.transform);
+       wolf = ResourceManager.Instance.Instantiate("Wolf.prefab", monster.transform);
+       pig = ResourceManager.Instance.Instantiate("Pig.prefab", monster.transform);
+
 
     }
 }
